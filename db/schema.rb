c6d2981635d77540b98eb50d78bb0bd20086266f
Tyler Porter <tylerpporter@gmail.com>
2020-04-17 16:29:34 -0600

    Create bulk_discounts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190724161751) do
+ActiveRecord::Schema.define(version: 20200417222647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bulk_discounts", force: :cascade do |t|
+    t.integer "discount_percentage"
+    t.integer "item_threshold"
+    t.bigint "merchant_id"
+    t.index ["merchant_id"], name: "index_bulk_discounts_on_merchant_id"
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -84,6 +91,7 @@ ActiveRecord::Schema.define(version: 20190724161751) do
     t.index ["merchant_id"], name: "index_users_on_merchant_id"
   end
 
+  add_foreign_key "bulk_discounts", "merchants"
   add_foreign_key "items", "merchants"
   add_foreign_key "order_items", "items"
   add_foreign_key "order_items", "orders"
